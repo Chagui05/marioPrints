1785ceebe19a2bc8d0516c0251914ea1f747e681
Santiago Chaves <santichavesg@gmail.com>
2023-08-18 11:50:21 -0600

    cambios solicitados a obstaculo

--- cargame/src/track/Obstaculo.java
+++ cargame/src/track/Obstaculo.java
@@ -1,5 +1,14 @@
 package track;
 
 public class Obstaculo {
+	private int damage;
+	private int height;
+	private String type;
+	
+	public Obstaculo(int pDamage, int pHeight, String pType) {
+		this.damage = pDamage;
+		this.height = pHeight;
+		this.type = pType;
+	}
 
 }
